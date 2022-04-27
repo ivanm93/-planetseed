a5f1e5a27e43e71afef943f859b2bcc986ca19ef
unknown <ariel1996frutos@gmail.com>
2022-04-26 22:42:50 -0300

    usuario crud servicios

--- src/main/java/planetseed/proyectofinal/servicios/UsuarioServicio.java
+++ src/main/java/planetseed/proyectofinal/servicios/UsuarioServicio.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package planetseed.proyectofinal.servicios;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -28,69 +28,118 @@ import planetseed.proyectofinal.enumeraciones.Role;
 import planetseed.proyectofinal.errores.ErrorServicio;
 import planetseed.proyectofinal.repositorios.UsuarioRepo;
 
-
 @Service
 public class UsuarioServicio implements UserDetailsService {
 
     @Autowired
     private UsuarioRepo usuarioRepo;
-    
+
     @Autowired
     private ImagenServicio imagenServicio;
-    
+
     @Transactional
-    public Usuario crear(String nombre, String apellido, Integer edad, String email, 
+    public Usuario crear(String nombre, String apellido, Integer edad, String email,
             String password, MultipartFile archivo) throws ErrorServicio {
 
         validar(nombre, apellido, edad, email, password);
 
         Usuario u = new Usuario();
-        
+
         u.setNombre(nombre);
         u.setApellido(apellido);
         u.setEdad(edad);
         u.setEmail(email);
-        
+
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         u.setPassword(encoder.encode(password));
-        
+
         Imagen imagen = imagenServicio.guardar(archivo);
         u.setImagen(imagen);
-        
+
         u.setRole(Role.USER);
-        
+
         u.setPuntos(0);
-        
+
         u.setAlta(true);
 
         return usuarioRepo.save(u);
     }
-    
+
+    @Transactional
+    public Usuario editar(String id, String nombre, String apellido, Integer edad,
+            String email, String password, MultipartFile archivo) throws ErrorServicio {
+
+        validar(nombre, apellido, edad, email, password);
+        
+        Optional<Usuario> respuesta = usuarioRepo.findById(id);
+        if (respuesta.isPresent()) {
+            Usuario u = respuesta.get();
+
+            u.setNombre(nombre);
+            u.setApellido(apellido);
+            u.setEdad(edad);
+            u.setEmail(email);
+
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            u.setPassword(encoder.encode(password));
+
+            Imagen imagen = imagenServicio.guardar(archivo);
+            u.setImagen(imagen);
+       
+            return usuarioRepo.save(u);           
+        } else {
+            throw new ErrorServicio("No se ha encontrado el usuario");
+        }
+    }
+
+    @Transactional
+    public void darBaja(String id) throws Exception {
+        Optional<Usuario> respuesta = usuarioRepo.findById(id);
+        if (respuesta.isPresent()) {
+            Usuario u = respuesta.get();
+            u.setAlta(false);
+            usuarioRepo.save(u);
+        } else {
+            throw new Exception("No se ha encontrado el usuario");
+        }
+    }
+
+    @Transactional
+    public void eliminar(String id) throws Exception {
+        Optional<Usuario> respuesta = usuarioRepo.findById(id);
+        if (respuesta.isPresent()) {
+            Usuario u = respuesta.get();
+            usuarioRepo.delete(u);
+        } else {
+            throw new Exception("No se ha encontrado el usuario");
+        }
+    }
+
     @Transactional(readOnly = true)
     public Usuario buscarPorEmail(String email) {
         return usuarioRepo.buscarPorEmail(email);
     }
-    
-    public void validar(String nombre, String apellido, Integer edad, String email, 
+
+    public void validar(String nombre, String apellido, Integer edad, String email,
             String password) throws ErrorServicio {
-             
+
         if (nombre == null || nombre.isEmpty()) {
             throw new ErrorServicio("El nombre es inválido");
         }
         if (apellido == null || apellido.isEmpty()) {
             throw new ErrorServicio("El apellido es inválido");
-        }      
+        }
         if (edad == null || edad < 1) {
             throw new ErrorServicio("La edad es inválida");
-        }      
+        }
         if (email == null || email.isEmpty()) {
             throw new ErrorServicio("El mail es inválido");
         }
-        if (password == null || password.isEmpty() || password.length()<3) {
+        if (password == null || password.isEmpty() || password.length() < 3) {
             throw new ErrorServicio("La contraseña es inválida");
         }
     }
-    
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Usuario u = buscarPorEmail(email);
@@ -99,7 +148,7 @@ public class UsuarioServicio implements UserDetailsService {
 
             GrantedAuthority p1 = new SimpleGrantedAuthority("ROLE_" + u.getRole());
             permisos.add(p1);
-            
+
             ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
             HttpSession session = attr.getRequest().getSession(true);
             session.setAttribute("usuariosession", u);
@@ -112,5 +161,4 @@ public class UsuarioServicio implements UserDetailsService {
         }
     }
 
-    
 }
